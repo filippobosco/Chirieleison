df289edf479a6b812313f93da168061758fce3f2
filippobosco <filippo.bosco@reddoak.com>
2026-01-27 12:18:38 +0100

    Rimossa configurazione turbo non valida da next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    turbo: {
-      root: path.join(__dirname),
-    },
-  },
 };
 
 export default nextConfig;
